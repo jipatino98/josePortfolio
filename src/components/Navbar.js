3eda808f0c251c49d7ce8e3f3ee899a88a092ea7
Jose Patino <120534408+jipatino98@users.noreply.github.com>
2023-09-12 17:13:02 -0700

    Created an array to hold Links and map over them to render them

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,51 +1,62 @@
 import { Link } from "gatsby";
+import { globalHistory } from "@reach/router";
 import React from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 export default function Navbar() {
-  const [nav, setNav] = React.useState(true);
-  console.log(nav);
+  const [nav, setNav] = React.useState(false);
+  const navLinks = [
+    { label: "Home", to: "/" },
+    { label: "Projects", to: "/projects/" },
+    { label: "About", to: "/about/" },
+    { label: "Contact", to: "/contact/" },
+  ];
+  const pathname = globalHistory.location.pathname;
 
   return (
     <nav className="flex justify-between items-center my-0 mx-auto p-7 bg-black text-white">
       <h1>JPatino</h1>
 
-      <div className="md:flex justify-between gap-3 hidden">
-        <Link
-          className="hover:scale-105 duration-200 hover:text-red-600"
-          to="/"
-        >
-          Home
-        </Link>
+      {/*Links on Desktop*/}
+      <ul role="presentation" className=" md:flex justify-between gap-3 hidden">
+        {navLinks.map((link) => (
+          <li
+            key={link.label}
+            role="presentation"
+            className={`hover:scale-105 duration-200 hover:underline ${
+              pathname === link.to ? "text-red-600" : " "
+            }`}
+          >
+            <Link to={link.to}>{link.label}</Link>
+          </li>
+        ))}
+      </ul>
 
-        <Link
-          className="hover:scale-105 duration-200 hover:text-red-600"
-          to="/projects"
-        >
-          Projects
-        </Link>
-
-        <Link
-          className="hover:scale-105 duration-200 hover:text-red-600"
-          to="/about"
-        >
-          About
-        </Link>
-
-        <Link
-          className="hover:scale-105 duration-200 hover:text-red-600"
-          to="/contact"
-        >
-          Contact
-        </Link>
-      </div>
-
-      <div
+      {/*Burger Menu*/}
+      <button
         onClick={() => setNav(!nav)}
-        className="cursor-pointer z-10 text-gray-300 md:hidden"
+        onKeyDown={() => setNav(!nav)}
+        className="cursor-pointer z-10 text-gray-300 md:hidden border-red-50"
       >
-        {nav ? <FaBars size={30} /> : <FaTimes size={30} />}
-      </div>
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </button>
+
+      {/*Mobile Menu*/}
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-400">
+          {navLinks.map((link) => (
+            <li
+              key={link.label}
+              role="presentation"
+              className={`px-4 cursor-pointer capitalize mb-3 text-3xl ${
+                pathname === link.to ? "text-red-600" : " "
+              }`}
+            >
+              <Link to={link.to}>{link.label}</Link>
+            </li>
+          ))}
+        </ul>
+      )}
     </nav>
   );
 }
